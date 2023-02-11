dedcf8eee99ea806f3b0c82d7c935ef9975d7deb
Zach Kissane <zkissane@gmail.com>
2023-02-10 22:44:28 -0600

    make index.js a little neater

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = new express()
-app.use(require('body-parser').json())
+app.use(express.json())
 require('./routes/')(app)
 require('dotenv').config()
 const port = process.env.PORT || 3000
@@ -8,14 +8,14 @@ const cors = require('cors')
 let corsOptions = {
     origin: `http://localhost:${port}`
 }
-app.use(cors(corsOptions))
+app.use(cors(corsOptions)) 
 require('./helpers/mongoose-config').dbconnect()
 
 const db = require("./models")
 db.initial()
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}` )
+    console.log(`App running in ${process.env.NODE_ENV}, listening on port ${port}` )
 })
 
 module.exports = app
