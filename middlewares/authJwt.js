c97c122a2375d4274e19c40d44046afe3b7082f5
Zach Kissane <zkissane@gmail.com>
2023-02-21 04:38:35 +0000

    middleware now looks up user from the id in the token and stuffs it in the request as currentUser

--- middlewares/authJwt.js
+++ middlewares/authJwt.js
@@ -1,58 +1,68 @@
-const jwt = require("jsonwebtoken")
-const config = require("../config/auth.config")
-const db = require("../models")
-const User = db.user
-const Role = db.role
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config");
+const { user } = require("../models");
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
 
 verifyToken = (req, res, next) => {
-    let token = req.headers["x-access-token"] || (req.headers['authorization']?.split(' ')[1])
+  let token =
+    req.headers["x-access-token"] ||
+    req.headers["authorization"]?.split(" ")[1];
 
-    if(!token) {
-        return res.status(403).send({ message: 'No token provided'})
-    }
-
-    jwt.verify(token, config.secret, (err, decoded) => {
-        if (err) { return res.status(401).send({message: "Unauthorized"})}
-        req.userId = decoded.id
-        next()
-    })
-}
-
-isAdmin = (req, res, next) => {
-    User.findById(req.userId).exec((err, user) => {
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
-  
-      Role.find(
-        {
-          _id: { $in: user.roles }
-        },
-        (err, roles) => {
-          if (err) {
-            res.status(500).send({ message: err });
-            return;
-          }
-  
-          for (let i = 0; i < roles.length; i++) {
-            if (roles[i].name === "admin") {
-              next();
-              return;
-            }
-          }
-  
-          res.status(403).send({ message: "Require Admin Role!" });
-          return;
-        }
-      );
-    });
-  };
-  
-
-  const authJwt = {
-    verifyToken,
-    isAdmin,
+  if (!token) {
+    return res.status(403).send({ message: "No token provided" });
   }
 
-  module.exports = authJwt
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+    req.currentUserId = decoded.id;
+    next();
+  });
+};
+
+lookupCurrentUser = async (req, res, next) => {
+  try {
+    let userFromToken = await User.findById(req.currentUserId).orFail();
+    req.currentUser = userFromToken;
+    next();
+  } catch (error) {
+    console.log(`User lookup failed: ${error}`);
+    return res.status(404).send({ message: "Lookup failed." });
+  }
+};
+
+lookupCurrentUsersRoles = async (req, res, next) =>{
+  currentUser = req.currentUser;
+  try{
+    req.currentUserRoles = await Role.find({ _id: { $in: currentUser.roles } });
+    next();
+  }
+  catch(error) {
+    console.log(`lookupCurrentUserRoles failed: ${error}`);
+    return res.status(403).send({message: "User requires authorization but doesn't have it"})
+  }
+}
+
+hasRole = (role) => {
+  return (req, res, next) => {
+    if (req.currentUserRoles?.find((element) => element.name === role)) {
+      next();
+    }
+    else {
+      console.log(`hasRole: User ${req.currentUserId} needs role ${role} but doesn't have it.`)
+      return res.status(403).send({message: 'Unauthorized'})
+    }
+  }
+}
+
+const authJwt = {
+  verifyToken,
+  lookupCurrentUser,
+  lookupCurrentUsersRoles,
+  hasRole,
+};
+
+module.exports = authJwt;
