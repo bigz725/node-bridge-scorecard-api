b6cd911a6e54c45cc0a40059f1dacbbd5d3ce0f9
Zach Kissane <zkissane@gmail.com>
2023-03-23 04:05:32 +0000

    Remove test from url

--- routes/session.routes.js
+++ routes/session.routes.js
@@ -10,11 +10,11 @@ const fullSelfOrAdminStack = [
 ]
 
 module.exports = function(app) {
-    app.get("/api/test/sessions", [verifyToken, lookupCurrentUser], controller.getSessions)
-    app.get("/api/test/sessions/:id", [verifyToken, lookupCurrentUser, lookupTargetSession], controller.getSession)
-    app.post("/api/test/sessions", [verifyToken, lookupCurrentUser], controller.createSession)
-    app.patch("/api/test/sessions/:id", [verifyToken, lookupCurrentUser, lookupTargetSession], controller.patchSession)
-    app.delete("/api/test/sessions/:id", [verifyToken, lookupCurrentUser], controller.deleteSession)
-    app.post("/api/test/sessions/:id/boards", [verifyToken, lookupCurrentUser, lookupTargetSession], controller.addBoard)
-    app.patch("/api/test/sessions/:id/boards/:boardId", [verifyToken, lookupCurrentUser, lookupTargetSession], controller.patchBoard)
+    app.get("/api/sessions", [verifyToken, lookupCurrentUser], controller.getSessions)
+    app.get("/api/sessions/:id", [verifyToken, lookupCurrentUser, lookupTargetSession], controller.getSession)
+    app.post("/api/sessions", [verifyToken, lookupCurrentUser], controller.createSession)
+    app.patch("/api/sessions/:id", [verifyToken, lookupCurrentUser, lookupTargetSession], controller.patchSession)
+    app.delete("/api/sessions/:id", [verifyToken, lookupCurrentUser], controller.deleteSession)
+    app.post("/api/sessions/:id/boards", [verifyToken, lookupCurrentUser, lookupTargetSession], controller.addBoard)
+    app.patch("/api/sessions/:id/boards/:boardId", [verifyToken, lookupCurrentUser, lookupTargetSession], controller.patchBoard)
 }
