d9feff5b1234ad637d208707912c2a0f26e6e47c
Zach Kissane <zkissane@gmail.com>
2023-08-13 20:11:24 +0000

    check for a secret file at config/secret.key or

--- config/auth.config.js
+++ config/auth.config.js
@@ -1,8 +1,7 @@
 require('dotenv').config()
-const util = require('util')
 const crypto = require('crypto')
-
-//const randomBytesAsync = util.promisify(crypto.randomBytes);
+const fs = require('fs')
+const logger = require('../logger').textLogger
 const saltByteLength = parseInt(process.env.SALT_BYTE_LENGTH) || 48
 
 exports.salt = () => {
@@ -10,5 +9,40 @@ exports.salt = () => {
     return retval
 }
 
-exports.secret = process.env.SECRET || "lskdjflskdjflkwjsfiwjef"
+jwtSecretFromFile = (fileName=(process.env.SECRET_FILE || "config/secret.key")) => {
+    try {
+        const data = fs.readFileSync(fileName, 'utf8')
+        logger.info(`Using JWT secret from file ${fileName}`)
+        return data
+    }
+    catch (err) {
+        logger.warn(`Unable to read JWT secret from file ${fileName}`)
+        logger.warn(err)
+    }
+}
+
+requireConcreteSecret = () => {
+    //do not require a secret for any dev, test, or local environment
+    //if the environment is not set, assume production
+    regex = /(dev*|test*|local*)/i
+    return !regex.test(process.env.NODE_ENV || "production")
+}
+
+let secret = process.env.SECRET || jwtSecretFromFile()
+ 
+if (secret == null) {
+    if (requireConcreteSecret() ) {
+        logger.error("Attempting to run a deployment without a JWT secret")
+        logger.error("If you get this message in a development environment,")
+        logger.error("set the environment variable NODE_ENV to one of 'dev', 'test', or 'local'")
+        throw new Error("Attempting to run a deployment without a JWT secret")
+    }
+    else {
+        logger.warn("No JWT secret found, using a dummy secret")
+        logger.warn("This is only appropriate for development environments")
+    }
+}
+
+exports.secret = secret || "for development only"
 exports.saltByteLength = saltByteLength
+
